1030ac65299e21716b0d0472eba3cb0ffcb5faa4
Jeffery <wangjiehui11235@163.com>
2019-05-28 17:10:59 +0800

    code update

--- improved_reversal.py
+++ improved_reversal.py
@@ -8,59 +8,58 @@ import multiprocessing
 import sqlalchemy as sa
 from sqlalchemy import select, and_, func
 from PyFin.api import DateUtilities
-from models import Market5MinBar,Market
+from models import Market5MinBar, Market
 
-def calc_factor_by_day(trade_date: datetime.datetime) -> pd.DataFrame:
-    table = Market5MinBar
-    conn = sa.create_engine(config.DX_DB)
-    query = select([table.trade_date,table.code,table.bar_time,table.close_price,
-                    ]).where(and_(table.trade_date==trade_date))
-    
-    df = pd.read_sql(query,conn)
-    grouped = df.groupby('code')
-    improved_reversal_df = pd.DataFrame(columns=['improved_reversal'])
-    for k, g in grouped:
-        g = g.sort_values(by='bar_time', ascending=True)
-        improved_reversal = (g.close_price.iloc[-1]-g.close_price.iloc[6])/g.close_price.iloc[6]
-        improved_reversal_df.loc[k, :] = improved_reversal
-    improved_reversal_df = improved_reversal_df
-    improved_reversal_df.index.name = 'code'
-    improved_reversal_df['trade_date'] = trade_date
-    return improved_reversal_df
+def calc_factor_by_day(unit: list):
+    k = unit[0]
+    g = unit[1]
+    g = g.sort_values(by='bar_time', ascending=True)
+    improved_reversal = (g.close_price.iloc[-1]-g.close_price.iloc[6])/g.close_price.iloc[6]
+    return {'improved_reversal':improved_reversal,
+            'code':k[1],'trade_date':k[0]}
 
-def calc_factor_by_code(params):
-    k = params[0]
-    g = params[1]
-    n_windows = params[2]
+def calc_factor_by_code(unit: list):
+    k = unit[0]
+    g = unit[1]
+    n_windows = unit[2]
     g = g.sort_values(by='trade_date', ascending=True)
-    g['improved_reversal'] = g['improved_reversal'].rolling(window=n_windows).mean().shift(1)
-    g['code'] = k
-    return g
+    g[str(n_windows)+'_improved_reversal'] = g['improved_reversal'].rolling(window=n_windows).mean().shift(1)
+    return g.loc[:,['trade_date','code',str(n_windows)+'_improved_reversal']].dropna()
 
 def calc_factor(begin_date: datetime.datetime,
                end_date: datetime.datetime) -> pd.DataFrame:
-    n_windows = 4
-    trade_date_list = DateUtilities.makeSchedule(begin_date,end_date,'1b','china.sse')
+    # param for mean in time
+    n_windows = 4 
+    table = Market5MinBar
+    conn = sa.create_engine('postgresql+psycopg2://alpha:alpha@180.166.26.82:8889/alpha')
     
-    res = []
+    # bars for 5-mins
+    query = select([Market5MinBar.trade_date,Market5MinBar.code,Market5MinBar.bar_time,Market5MinBar.close_price,
+                    ]).where(and_(Market5MinBar.trade_date >= begin_date,
+                                                           Market5MinBar.trade_date <= end_date))
+    res = pd.read_sql(query, conn)
+    grouped_list = []
+    grouped = res.groupby(['trade_date','code'])
+
+    for k, g in grouped:
+        grouped_list.append([k,g])
+    
     cpus = multiprocessing.cpu_count()
     with multiprocessing.Pool(processes=cpus) as p:
-        res = p.map(calc_factor_by_day, trade_date_list)
-    res = pd.concat(res).reset_index()
-
-    grouped = res.groupby('code')
+        improved_reversal_res = p.map(calc_factor_by_day, grouped_list)
+    improved_reversal_df= pd.DataFrame(improved_reversal_res)
+    
+    # calculate rolling mean by date
+    grouped = improved_reversal_df.groupby('code')
     grouped_list = []
     for k, g in grouped:
         grouped_list.append([k,g,n_windows])
-    cpus = multiprocessing.cpu_count()
     with multiprocessing.Pool(processes=cpus) as p:
         improved_reversal_res = p.map(calc_factor_by_code, grouped_list)
-        
     improved_reversal_df = pd.concat(improved_reversal_res).reset_index(drop=True)
-    improved_reversal_df = improved_reversal_df.dropna()
-    return improved_reversal_df.loc[:, ["trade_date", "code", "improved_reversal"]]
+    return improved_reversal_df
 
 if __name__ == '__main__':
-    begin_date = datetime.datetime(2019, 1, 1)
-    end_date = datetime.datetime(2019, 1, 10)
-    print(calc_factor(begin_date,end_date))
+    begin_date = datetime.datetime(2019,1,1)
+    end_date = datetime.datetime(2019,2,10)
+    print(calc_factor(begin_date, end_date))
--- trend_strength.py
+++ trend_strength.py
@@ -8,59 +8,58 @@ import multiprocessing
 import sqlalchemy as sa
 from sqlalchemy import select, and_, func
 from PyFin.api import DateUtilities
-from models import Market5MinBar,Market
+from models import Market5MinBar, Market
 
-def calc_factor_by_day(trade_date: datetime.datetime) -> pd.DataFrame:
-    table = Market5MinBar
-    conn = sa.create_engine(config.DX_DB)
-    query = select([table.trade_date,table.code,table.bar_time,table.close_price,
-                    ]).where(and_(table.trade_date==trade_date))
-    
-    df = pd.read_sql(query,conn)
-    grouped = df.groupby('code')
-    trend_strength_df = pd.DataFrame(columns=['trend_strength'])
-    for k, g in grouped:
-        g = g.sort_values(by='bar_time', ascending=True)
-        trend_strength = (g.close_price.iloc[-1]-g.close_price.iloc[0])/g.close_price.diff().fillna(0).map(np.abs).sum()
-        trend_strength_df.loc[k, :] = trend_strength
-    trend_strength_df = trend_strength_df
-    trend_strength_df.index.name = 'code'
-    trend_strength_df['trade_date'] = trade_date
-    return trend_strength_df
+def calc_factor_by_day(unit: list):
+    k = unit[0]
+    g = unit[1]
+    g = g.sort_values(by='bar_time', ascending=True)
+    trend_strength = (g.close_price.iloc[-1]-g.close_price.iloc[0])/g.close_price.diff().fillna(0).map(np.abs).sum()
+    return {'trend_strength':trend_strength, 
+           'code':k[1],'trade_date':k[0]}
 
-def calc_factor_by_code(params):
-    k = params[0]
-    g = params[1]
-    n_windows = params[2]
+def calc_factor_by_code(unit: list):
+    k = unit[0]
+    g = unit[1]
+    n_windows = unit[2]
     g = g.sort_values(by='trade_date', ascending=True)
-    g['trend_strength'] = g['trend_strength'].rolling(window=n_windows).mean().shift(1)
-    g['code'] = k
-    return g
+    g[str(n_windows)+'_trend_strength'] = g['trend_strength'].rolling(window=n_windows).mean().shift(1)
+    return g.loc[:,['trade_date','code',str(n_windows)+'_trend_strength']].dropna()
 
 def calc_factor(begin_date: datetime.datetime,
                end_date: datetime.datetime) -> pd.DataFrame:
-    n_windows = 4
-    trade_date_list = DateUtilities.makeSchedule(begin_date,end_date,'1b','china.sse')
+    # param for mean in time
+    n_windows = 4 
+    table = Market5MinBar
+    conn = sa.create_engine('postgresql+psycopg2://alpha:alpha@180.166.26.82:8889/alpha')
     
-    res = []
+    # bars for 5-mins
+    query = select([Market5MinBar.trade_date,Market5MinBar.code,Market5MinBar.bar_time,Market5MinBar.close_price,
+                    ]).where(and_(Market5MinBar.trade_date >= begin_date,
+                                                           Market5MinBar.trade_date <= end_date))
+    res = pd.read_sql(query, conn)
+    grouped_list = []
+    grouped = res.groupby(['trade_date','code'])
+
+    for k, g in grouped:
+        grouped_list.append([k,g])
+        
     cpus = multiprocessing.cpu_count()
     with multiprocessing.Pool(processes=cpus) as p:
-        res = p.map(calc_factor_by_day, trade_date_list)
-    res = pd.concat(res).reset_index()
+        trend_strength_res = p.map(calc_factor_by_day, grouped_list)
+    trend_strength_df= pd.DataFrame(trend_strength_res)
     
-    grouped = res.groupby('code')
+    # calculate rolling mean by date     
+    grouped = trend_strength_df.groupby('code')
     grouped_list = []
-    for k, g in grouped:
+    for k,g in grouped:
         grouped_list.append([k,g,n_windows])
-    cpus = multiprocessing.cpu_count()
     with multiprocessing.Pool(processes=cpus) as p:
         trend_strength_res = p.map(calc_factor_by_code, grouped_list)
-
     trend_strength_df = pd.concat(trend_strength_res).reset_index(drop=True)
-    trend_strength_df = trend_strength_df.dropna()
-    return trend_strength_df.loc[:, ["trade_date", "code", "trend_strength"]]
+    return trend_strength_df
 
 if __name__ == '__main__':
-    begin_date = datetime.datetime(2019, 1, 1)
-    end_date = datetime.datetime(2019, 1, 10)
-    print(calc_factor(begin_date,end_date))
+    begin_date = datetime.datetime(2019,1,1)
+    end_date = datetime.datetime(2019,2,10)
+    print(calc_factor(begin_date, end_date))
