315b124d3e9be8728ed881b324b326833f675be7
Jeffery <wangjiehui11235@163.com>
2019-05-29 11:24:16 +0800

    trade_dates revised

--- flow_in_ratio1.py
+++ flow_in_ratio1.py
@@ -7,6 +7,7 @@ import numpy as np
 import multiprocessing
 import sqlalchemy as sa
 from sqlalchemy import select, and_, func
+from dateutil.relativedelta import relativedelta
 from PyFin.api import DateUtilities
 from models import Market5MinBar, Market
 
@@ -30,17 +31,24 @@ def calc_factor_by_code(unit: list):
     return g.loc[:,['trade_date','code',str(n_windows)+'_flow_in_ratio1']].dropna()
 
 def calc_factor(begin_date: datetime.datetime,
-               end_date: datetime.datetime) -> pd.DataFrame:
+               end_date: datetime.datetime,
+               **kwargs) -> pd.DataFrame:
     # param for mean
-    n_windows = 4
-    conn = sa.create_engine(config.DX_DB)
+    n_windows = kwargs['windows']
+    conn = sa.create_engine('postgresql+psycopg2://alpha:alpha@180.166.26.82:8889/alpha')
+    
+    # n_windows more days needed to prevent NaNs during rolling calcaulation
+    temp_trade_date_list = DateUtilities.makeSchedule(begin_date-relativedelta(days=int(2 * n_windows)), 
+                                                      begin_date,'1b','china.sse')
+    temp_trade_date_list.sort(reverse=False)
+    temp_trade_date_list = temp_trade_date_list[-n_windows:]
     
     # bars for 5-mins
     table = Market5MinBar
     query = select([Market5MinBar.trade_date,Market5MinBar.code,
                     Market5MinBar.bar_time,Market5MinBar.close_price,
                     Market5MinBar.total_volume, Market5MinBar.total_value
-                   ]).where(and_(Market5MinBar.trade_date >= begin_date,
+                   ]).where(and_(Market5MinBar.trade_date >= temp_trade_date_list[0],
                                                            Market5MinBar.trade_date <= end_date))
     res = pd.read_sql(query, conn)
     grouped_list = []
@@ -57,7 +65,7 @@ def calc_factor(begin_date: datetime.datetime,
     # daily value
     table2 = Market
     query2 = select([table2.trade_date,table2.code,table2.turnoverValue
-                    ]).where(and_(Market.trade_date >= begin_date,
+                    ]).where(and_(Market.trade_date >= temp_trade_date_list[0],
                                                            Market.trade_date <= end_date))
     res2 = pd.read_sql(query2,conn)
     flow_in_df = flow_in_df.merge(res2, how='inner', on=['trade_date','code'])
@@ -76,4 +84,4 @@ def calc_factor(begin_date: datetime.datetime,
 if __name__ == '__main__':
     begin_date = datetime.datetime(2019,1,1)
     end_date = datetime.datetime(2019,1,10)
-    print(calc_factor(begin_date, end_date))
+    print(calc_factor(begin_date, end_date, windows=4))
--- trend_strength.py
+++ trend_strength.py
@@ -7,8 +7,9 @@ import numpy as np
 import multiprocessing
 import sqlalchemy as sa
 from sqlalchemy import select, and_, func
+from dateutil.relativedelta import relativedelta
 from PyFin.api import DateUtilities
-from models import Market5MinBar, Market
+from models import Market5MinBar,Market
 
 def calc_factor_by_day(unit: list):
     k = unit[0]
@@ -27,15 +28,24 @@ def calc_factor_by_code(unit: list):
     return g.loc[:,['trade_date','code',str(n_windows)+'_trend_strength']].dropna()
 
 def calc_factor(begin_date: datetime.datetime,
-               end_date: datetime.datetime) -> pd.DataFrame:
-    # param for mean in time
-    n_windows = 4 
+               end_date: datetime.datetime,
+               **kwargs) -> pd.DataFrame:
+    # param for mean
+    n_windows = kwargs['windows']
     table = Market5MinBar
-    conn = sa.create_engine(config.DX_DB)
+    conn = sa.create_engine('postgresql+psycopg2://alpha:alpha@180.166.26.82:8889/alpha')
+    
+#     pdb.set_trace()    
+    # n_windows more days needed to prevent NaNs during rolling calcaulation
+    temp_trade_date_list = DateUtilities.makeSchedule(begin_date-relativedelta(days=int(2 * n_windows)), 
+                                                      begin_date,'1b','china.sse')
+    temp_trade_date_list.sort(reverse=False)
+    temp_trade_date_list = temp_trade_date_list[-n_windows:]
+    # trade_date_list = DateUtilities.makeSchedule(temp_trade_date_list[0],end_date,'1b','china.sse')
     
     # bars for 5-mins
     query = select([Market5MinBar.trade_date,Market5MinBar.code,Market5MinBar.bar_time,Market5MinBar.close_price,
-                    ]).where(and_(Market5MinBar.trade_date >= begin_date,
+                    ]).where(and_(Market5MinBar.trade_date >= temp_trade_date_list[0],
                                                            Market5MinBar.trade_date <= end_date))
     res = pd.read_sql(query, conn)
     grouped_list = []
@@ -62,4 +72,4 @@ def calc_factor(begin_date: datetime.datetime,
 if __name__ == '__main__':
     begin_date = datetime.datetime(2019,1,1)
     end_date = datetime.datetime(2019,1,10)
-    print(calc_factor(begin_date, end_date))
+    print(calc_factor(begin_date, end_date, windows=4))
